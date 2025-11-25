34bc42258c66622757a8e83c8c224ab89f7eb492
sese972010 <sese972010@126.com>
2025-11-25 23:00:53 +0800

    update: 你的更新内容

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    sourcemap: false
   }
 });
